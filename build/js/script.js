40a2effae5abdc18c2c3996722efe7fa5713e9bb
simplexxagency <ant@simplexx.agency>
2019-12-13 14:28:05 +0200

    second commit

--- build/js/script.js
+++ build/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     // Form Mask
     jQuery(function ($) {
         $('#phone').mask('+9 (999) 999-9999');
-
+        $('#phone2').mask('+9 (999) 999-9999');
     });
 
     // Tooltip in Header
@@ -24,6 +24,67 @@ $(document).ready(function () {
   $(window).scroll(function () {
       let top = $(this).scrollTop();
 
-      $header.toggleClass('active', top > ($hero.height() / 2));
+      $header.toggleClass('active', top > ($hero.height() * 0.15));
   });
+
+  // Menu in Header
+  let $btn = $('.hero__mobile-line');
+  let $navMenu = $('.hero__mobile-menu');
+
+  $('.hero__mobile-btn').on('click', function () {
+      if (!($btn.hasClass('active'))) {
+          $btn.addClass('active');
+          $navMenu.addClass('active');
+          $('body').addClass('active');
+      } else {
+          $btn.removeClass('active');
+          $navMenu.removeClass('active');
+          $('body').removeClass('active');
+      }
+
+  });
+
+  $('.hero__mobile-item a').on('click', function () {
+      $('.hero__mobile-line').removeClass('active');
+      $('.hero__mobile-menu').removeClass('active');
+      $('body').removeClass('active');
+  });
+
+  // Smooth scroll
+  $('a[href^="#"]').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 1000);
+    $('body'). removeClass('active');
+    return false;
+});
+
+  // Slick slider in section Test
+  function slickifyTest() {
+    $('.test__slider').slick({
+      dots: true,
+      arrows: true,
+      // mobileFirst: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      swipe: true,
+      nextArrow: '.test__arrow-right',
+      prevArrow: '.test__arrow-left',
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
+    });
+  }
+
+  slickifyTest();
 });
